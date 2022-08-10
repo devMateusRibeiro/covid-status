a8ca618b48355e0c004a3e7ff86069ca676108fb
devMateusRibeiro <contato@mateusribeiro.dev.br>
2022-08-10 10:57:12 -0300

    remoção fundo branco

--- gui.py
+++ gui.py
@@ -9,7 +9,7 @@ fonte_branca = {"fg":"white"}
 fundo_branco = {"fg":"black", "bg":"white"}
 
 ##PRIMEIRA LINHA - Título
-texto_titulo = Label(janela, text="Estatísticas do COVID-19", **fundo_branco, font=(None, 20))
+texto_titulo = Label(janela, text="Estatísticas do COVID-19", font=(None, 20))
 texto_titulo.grid(column=1, row=0, **padding10x)
 
 ##SEGUNDA LINHA - Estatísticas
@@ -23,17 +23,16 @@ texto_mortes = Label(janela, text="Mortes", **fonte_branca, bg="red", **fonte_pa
 texto_mortes.grid(column=2, row=1, **padding10x)
 
 ##TERCEIRA LINHA - Output dos valores
-quantidade_infectados = Label(janela, text="----", **fundo_branco, **fonte_padrao)
+quantidade_infectados = Label(janela, text="----", **fonte_padrao)
 quantidade_infectados.grid(column=0, row=2, **padding10x)
 
-quantidade_recuperados = Label(janela, text="----", **fundo_branco, **fonte_padrao)
+quantidade_recuperados = Label(janela, text="----", **fonte_padrao)
 quantidade_recuperados.grid(column=1, row=2, **padding10x)
 
-quantidade_mortes = Label(janela, text="----", **fundo_branco, **fonte_padrao)
+quantidade_mortes = Label(janela, text="----", **fonte_padrao)
 quantidade_mortes.grid(column=2, row=2, **padding10x)
 
 ##QUARTA LINHA - Input do país
-
 botao = Button(janela, text="Consultar", **fonte_padrao)
 botao.grid(column=2, row=3, **padding10x)
 
